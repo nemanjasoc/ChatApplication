afa219d750b772276cac77794c6c5af1747723cf
Nemanja Soc <nemanjasoc@gmail.com>
2018-10-30 21:56:36 +0100

    show users

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		var myObj = JSON.parse(this.responseText);
-		document.getElementById('main-content').innerHTML = myObj;
+var parsedData = [];
+
+var myRequest = new XMLHttpRequest();
+myRequest.open("GET", "json/conversations.json", true);
+myRequest.onload = function() {
+	parsedData = JSON.parse(myRequest.responseText);
+	showUsers();
+}
+myRequest.send();
+
+function showUsers() {
+	for (var i = 0; i < parsedData.length; i++) {
+		var user = parsedData[i];
+
+		document.getElementById("aside").innerHTML += '<div id="user-image" onclick="showUserMessages()"><img src="img/'+ user.name +'.jpg"><div class="user-data">' +
+		'<div class="user-name">' + user.name +'</div><div class="user-status">'+ user.status +'</div></div></div>'
 	}
 }
-xmlhttp.open("GET", conversation.json, true);
-xmlhttp.send();
