a37e7bf30e01f8578c0f347e35548f19e6de5fb6
Nemanja Soc <nemanjasoc@gmail.com>
2018-11-02 21:44:20 +0100

    show user messages

--- js/script.js
+++ js/script.js
@@ -1,18 +1,77 @@
-var parsedData = [];
+var data = [];
 
 var myRequest = new XMLHttpRequest();
 myRequest.open("GET", "json/conversations.json", true);
 myRequest.onload = function() {
-	parsedData = JSON.parse(myRequest.responseText);
+	data = JSON.parse(myRequest.responseText);
 	showUsers();
 }
 myRequest.send();
 
 function showUsers() {
-	for (var i = 0; i < parsedData.length; i++) {
-		var user = parsedData[i];
+	var aside = document.getElementById("aside");
 
-		document.getElementById("aside").innerHTML += '<div id="user-image" onclick="showUserMessages()"><img src="img/'+ user.name +'.jpg"><div class="user-data">' +
-		'<div class="user-name">' + user.name +'</div><div class="user-status">'+ user.status +'</div></div></div>'
+	for (var i = 0; i < data.length; i++) {
+		var user = data[i];
+		
+		aside.innerHTML += '<div class="user-content" id="user-content-'+ i +'" onclick="showUserMessages('+ i +')">' +
+								'<img src="img/'+ user.name +'.jpg">' +
+								'<div class="user-data">' + 
+									'<div class="user-name">' + user.name +'</div>' +
+									'<div class="user-status">'+ user.status +'</div>' +
+								'</div>' +
+							'</div>'
 	}
+}
+
+function showUserMessages(currentUser) {
+	console.log("currentUser: ", currentUser);
+	var selectAll = document.querySelectorAll(".user-content");
+	var messagesContent = document.getElementById("messages-content");
+	var user;
+
+	for (var i = 0; i < selectAll.length; i++) {
+		var select = selectAll[i];
+	
+		if (select) {
+			select.classList.remove('active');
+		}
+	}
+	
+	document.getElementById(`user-content-${currentUser}`).classList.add("active");
+	
+	for (var i = 0; i < data.length; i++) {
+		if (i == currentUser) {
+			user = data[i];
+		}
+	}
+
+	resetUserMessages();
+
+	for (var j = 0; j < user.messages.length; j++) {
+		var message = user.messages[j];
+		var messagesType = message.type;
+		var classType = "";
+
+		if (messagesType == 'received') {
+			classType = 'received'
+		} else {
+			classType = 'sent'
+		}
+		
+		messagesContent.innerHTML +=`<div class="messages-type">` +
+										`<div class="messages-${classType}">` +
+											`<div class="message-${classType}-content">${message.content}</div>` +
+											`<div class="message-${classType}-date">` + new Date(message.time).toLocaleTimeString() +`</div>` +
+										`</div>` +
+									`</div>`													
+	}
+}
+
+function resetUserMessages() {
+	document.getElementById("messages-content").innerHTML = '';
+}
+
+function sendMessage() {
+	var inputValue = document.getElementById("text-message").value;
 }
